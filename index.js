754e56bb187c6f4070e161e7b4b45d4f63cfb977
Adrian Gonzalez <Adrgon@Adrians-MacBook-Pro.local>
2020-04-11 02:27:26 -0300

    Handle error

--- index.js
+++ index.js
@@ -5,57 +5,33 @@ const app = module.exports = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/api/timestamp', function(req, res) {
+  let date = new Date();
 
-app.get("/api/timestamp/", (req, res) => {
-  res.json({ unix: Date.now(), utc: Date() });
+  res.json({
+    "unix": date.getTime(),
+    "utc": date.toUTCString()
+  });
 });
 
-
-app.get("/api/timestamp/:date_string", (req, res) => {
+app.get('/api/timestamp/:date_string', function(req, res) {
   let dateString = req.params.date_string;
 
-  if (/\d{5,}/.test(dateString)) {
-    var dateInt = parseInt(dateString);
-    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  let date = (/\D/.test(dateString)) ? new Date(dateString) : new Date(parseInt(dateString));
+
+  if(date.toString() === "Invalid Date") {
+    res.json({
+      "error": date.toString()
+    })
   }
-
-  let dateObject = new Date(dateString);
-
-  if (dateObject.toString() === "Invalid Date") {
-    res.json({ error: "Invaid Date" });
-  } else {
-    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  else {
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
   }
 });
 
-
-/*
-app.get('/api/timestamp/:dataVal', function(req, res){
-    let dataVal = req.params.dataVal
-
-    const dataFormattingOptions = {
-        year: 'numeric',
-        month: 'long', 
-        day: 'numeric'
-    }
-  
-    if(isNaN(dataVal)){
-        var naturalDate = new Date(dataVal)
-        if (naturalDate.toString() === "Invalid Date") {
-          res.json({ error: "Invaid Date" });
-        } 
-        naturalDate = naturalDate.toLocaleDateString("en-us", dataFormattingOptions)    
-        var unixDate = new Date(dataVal).getTime()/1000
-    } else {
-        var unixDate = dataVal
-        var naturalDate = new Date(dataVal*1000)
-        naturalDate = naturalDate.toLocaleDateString("en.us", dataFormattingOptions)
-    }
-      
-    res.json({unix: unixDate, natural: naturalDate})
-})
-
-*/
 
 app.listen(3000, function(){
     console.log("Working...")
